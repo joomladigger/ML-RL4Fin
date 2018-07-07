86ed7cc023497e455786e326f838acaea83c7e78
OTreeWEN <tao.xing@qq.com>
2018-07-07 21:41:44 +0800

    Courser 1 / Week 1 / Practice
    Courser 1 / Week 1 / Practice

---
+++ ex1-1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  7 20:04:23 2018
+
+@author: otreewen
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn
+
+# Load the data
+import os
+os.chdir()
+
